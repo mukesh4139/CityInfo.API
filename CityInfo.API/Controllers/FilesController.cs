a9d7f89603d72a0fc48d8354083d240069a4d299
Mukesh Chaudhary <muchaudhary@microsoft.com>
2023-10-13 20:48:45 +0530

    add files controller

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtentionContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider    
+        )
+        {
+            _fileExtentionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            // look up the actual file, depending on the fileId
+            // demo code
+            var pathToFile = "dental.pdf";
+
+            // check whether the file exists
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtentionContentTypeProvider.TryGetContentType(pathToFile, out var contentType)) 
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return File(bytes, "application/pdf", Path.GetFileName(pathToFile));
+        }
+    }
+}

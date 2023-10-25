422368d71025cbc87a632d7e41e5ce30e978ee4f
Mukesh Chaudhary <muchaudhary@microsoft.com>
2023-10-25 10:24:19 +0530

    add entities

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,26 @@
+﻿using CityInfo.API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+               = new List<PointOfInterest>();
+
+        public City(string name)
+        { 
+            Name = name; 
+        }
+    }
+}

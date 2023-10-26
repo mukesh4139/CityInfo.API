9ee0fd40f9455ce6c70fb6075920fa5edeb4bff3
Mukesh Chaudhary <muchaudhary@microsoft.com>
2023-10-26 16:38:59 +0530

    add automapper

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -41,6 +41,9 @@ builder.Services.AddDbContext<CityInfoContext>(
         builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

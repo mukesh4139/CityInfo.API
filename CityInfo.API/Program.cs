97ee2ead44b0ed285491d2312d99925bf2099e55
Mukesh Chaudhary <muchaudhary@microsoft.com>
2023-10-27 11:25:20 +0530

    add policy

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -61,6 +61,16 @@ builder.Services.AddAuthentication("Bearer")
             };
         }
     );
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeFromAntwerp", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("city", "Antwerp");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

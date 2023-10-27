6fc3fd372b0f734836eda313222d42ad036c4c03
Mukesh Chaudhary <muchaudhary@microsoft.com>
2023-10-27 14:15:34 +0530

    add authentication to docs

--- CityInfo.API/Controllers/FilesController.cs
+++ CityInfo.API/Controllers/FilesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.StaticFiles;
 namespace CityInfo.API.Controllers
 {
     [Route("api/file")]
-    // [Authorize]
+    [Authorize]
     [ApiController]
     public class FilesController : ControllerBase
     {

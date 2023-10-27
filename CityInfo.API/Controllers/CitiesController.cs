0aaeb3886a9d2c7a09fcfc4956951969e21f3882
Mukesh Chaudhary <muchaudhary@microsoft.com>
2023-10-27 13:03:39 +0530

    add custom comment to all request codes

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -49,7 +49,11 @@ namespace CityInfo.API.Controllers
         /// <param name="id">The id of the City to get</param>
         /// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
         /// <returns>An IActionResult</returns>
+        /// <response code="200">Returns the requested city</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetCity(
             int id,
             bool includePointsOfInterest = false)
